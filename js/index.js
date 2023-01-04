0a9076cfd589e3bb92eb756229f2ab3fc8f42edd
Rowa <jrowah@gmail.com>
2023-01-04 18:53:23 +0300

    saves DOM node references

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-
+//saving DOM reference nodes
+const tiles = Array.from(document.querySelectorAll(".tile")); //1. Grabs all board titles and creates and array of tile
+const playerDisplay = document.querySelector(".display-player");
+const winnerAnnouncer = document.querySelector(".announcer");
